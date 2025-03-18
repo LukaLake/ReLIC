faeda7224f1e9d3c02b74c2f706a7ccf10544436
Liu Yuchen <861053440@qq.com>
2025-03-18 15:38:35 +0800

    Update option.py

--- code/AVA/option.py
+++ code/AVA/option.py
@@ -11,7 +11,7 @@ def init():
     parser.add_argument('--path_to_test_images', type=str, default=r'C:\Users\Administrator\Documents\GitHub\ReLIC\Images',
                         help='directory to images for prediction')
     
-    parser.add_argument('--image_name', type=str, default='69.jpg',
+    parser.add_argument('--image_name', type=str, default='Test7.png',
                         help='image name')
 
     parser.add_argument('--path_to_save_csv', type=str,default=r"C:\Users\Administrator\Documents\GitHub\ReLIC/data/AVA/",
@@ -31,13 +31,14 @@ def init():
 
     parser.add_argument('--init_lr', type=int, default=0.00003, help='learning_rate'
                         )
-    parser.add_argument('--num_epoch', type=int, default=30, help='epoch num for train'
+    parser.add_argument('--num_epoch', type=int, default=20, help='epoch num for train'
                         )
-    parser.add_argument('--batch_size', type=int,default=32,help='how many pictures to process one time'
+    parser.add_argument('--batch_size', type=int,default=16,help='how many pictures to process one time'
                         )
     parser.add_argument('--num_workers', type=int, default=8, help ='num_workers',
                         )
     parser.add_argument('--gpu_id', type=str, default='0', help='which gpu to use')
+    parser.add_argument('--training_mode', type=bool, default=True, help='training mode or not')
 
 
     args = parser.parse_args()
